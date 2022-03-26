ffe453c13d053f54a8e59dbcd3bff0c8930944d4
ST-Chara <chara_kill_sans@163.com>
2022-03-26 16:46:17 +0800

    Version and Gametype color add

--- src/game/version.h
+++ src/game/version.h
@@ -2,8 +2,10 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
+#ifndef NON_HASHED_H
 #include <game/generated/nethash.cpp>
 #define GAME_VERSION "0.6.4"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 static const char GAME_RELEASE_VERSION[8] = "0.6.4";
+#endif
 #endif
